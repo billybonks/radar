7b3acbe6a1532641ef0da7b4273ef009b03a696a
billybonksl <sebastienstettler@gmail.com>
2021-10-07 00:00:51 +0800

    implement create datasource route

--- app/router.js
+++ app/router.js
@@ -6,4 +6,10 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('datasources.create');
+
+  this.route('datasources', function () {
+    this.route('create');
+  });
+});
