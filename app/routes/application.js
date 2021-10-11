6d556fb7cd0756fbce43f80b9d6cbf605b97290e
billybonksl <sebastienstettler@gmail.com>
2021-10-11 23:34:36 +0800

    fix: rather just transition to an empty page

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,6 +2,11 @@ import Route from '@ember/routing/route';
 import { action } from '@ember/object';
 
 export default class ApplicationRoute extends Route {
+  beforeModel(transition) {
+    if (transition.to.name === 'index') {
+      this.transitionTo('graph.create');
+    }
+  }
   setupController(controller, model) {
     super.setupController(controller, model);
     this.controller = controller;
