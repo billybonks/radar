259fc92582d63909cfcd781dd4591b887628b3e4
billybonksl <sebastienstettler@gmail.com>
2021-10-12 20:07:12 +0800

    refactor all the variables

--- app/theme/default.js
+++ app/theme/default.js
@@ -1,12 +1,20 @@
 export default {
-  'base.font-size': '13px',
-  'base.background-color': '#252526',
-  'base.hover-color': '#2a2d2e',
-  'base.focus-color': '#062f4a',
-  'base.font-color': '#cccccc',
+  'font.h1-size': '26px',
+  'font.h1-weight': '600',
+  'font.b-size': '13px',
   'base.line-height': '1.4em',
   'base.font-family': '-apple-system,BlinkMacSystemFont,sans-serif',
-  'dropdown.background': '#151515',
-  'dropdown.foreground': '#3d424d',
-  'dropdown.border': '#333b12',
+  'hoverBackground': '#2a2d2e', //not sure whats the base version of this
+  'button.background': '#0e639c',
+  'button.secondaryBackground': '#3a3d41',
+  'button.foreground': 'white', //need to figure out how to do AND OR Things i think cannot do it in css3 but ye
+  'focusBorder': '1px solid #007fd4',
+  'foreground': '#cccccc',
+  'background': '#252526',
+  'selection.background': '#007fd4',
+  'descriptionForeground': '#cccccc',
+  'input.background': '#333333',
+  'input.foreground': '#cccccd',
+  'widget.shadow': 'rgba(0, 0, 0, 0.36) 0px 0px 8px 2px',
+  'sash.hoverBorder': '#007fd4',
 };
