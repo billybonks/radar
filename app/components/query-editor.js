5ff1bb5a708146f63dc4c22e66f9a11228df43db
billybonksl <sebastienstettler@gmail.com>
2021-10-31 22:48:31 +0800

    move options to a component

--- app/components/query-editor.js
+++ app/components/query-editor.js
@@ -1,15 +1,13 @@
 /* eslint-disable ember/no-side-effects */
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
-import { set } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
-import { A } from '@ember/array';
-import { computed } from '@ember/object';
 
 export default class QueryEditorComponent extends Component {
   @tracked results;
   @tracked columns;
+  @tracked optionsRows;
 
   @service store;
 
@@ -17,9 +15,6 @@ export default class QueryEditorComponent extends Component {
   async run(query) {
     this.__optionsRowsCache = null;
     await query.run();
-    let arrayNotifier = {};
-    this.optionsRows.pushObject(arrayNotifier);
-    this.optionsRows.removeObject(arrayNotifier);
   }
 
   @action
@@ -28,42 +23,16 @@ export default class QueryEditorComponent extends Component {
   }
 
   @action
-  changeDataSource(query, datasource) {
-    query.set('datasource', datasource);
+  async optionsUpdated(optionsRows) {
+    this.optionsRows = optionsRows;
   }
 
   @action
-  updateRow({ rowValue }, key, event) {
-    set(rowValue, key, event.target.checked);
-    let arrayNotifier = {};
-    this.optionsRows.pushObject(arrayNotifier);
-    this.optionsRows.removeObject(arrayNotifier);
-  }
-
-  @computed('args.query.rawColumns', 'args.query.viz.options')
-  get optionsRows() {
-    if (!this.args.query.rawColumns) {
-      return [];
-    }
-    let options = Object.fromEntries(
-      Object.keys(this.args.query.viz.options).map((key) => [key, true])
-    );
-    let optionsRows = this.args.query.rawColumns.map((col) => {
-      return {
-        field: col,
-        ...options,
-      };
-    });
-    return A(optionsRows);
-  }
-
-  get optionsColumns() {
-    let cols = ['field', ...Object.keys(this.args.query.viz.options)];
-    return cols.map((col) => ({ name: col, valuePath: col }));
+  changeDataSource(query, datasource) {
+    query.set('datasource', datasource);
   }
 
   get datasources() {
     return this.store.findAll('datasource');
   }
-
 }
--- app/components/viz/options.js
+++ app/components/viz/options.js
@@ -1,3 +1,40 @@
+/* eslint-disable ember/no-computed-properties-in-native-classes */
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { set, computed } from '@ember/object';
+import { A } from '@ember/array';
 
-export default class VizOptionsComponent extends Component {}
+export default class VizOptionsComponent extends Component {
+  @action
+  updateRow({ rowValue }, key, event) {
+    set(rowValue, key, event.target.checked);
+    let arrayNotifier = {};
+    this.optionsRows.pushObject(arrayNotifier);
+    this.optionsRows.removeObject(arrayNotifier);
+    this.args.optionsUpdated(this.optionsRows);
+  }
+
+  @computed('args.columns', 'args.vizOptions')
+  get optionsRows() {
+    if (!this.args.columns) {
+      return [];
+    }
+    let options = Object.fromEntries(
+      Object.keys(this.args.vizOptions).map((key) => [key, true])
+    );
+    let optionsRows = this.args.columns.map((col) => {
+      return {
+        field: col,
+        ...options,
+      };
+    });
+    let opts = A(optionsRows);
+    this.args.optionsUpdated(opts);
+    return opts;
+  }
+
+  get optionsColumns() {
+    let cols = ['field', ...Object.keys(this.args.vizOptions)];
+    return cols.map((col) => ({ name: col, valuePath: col }));
+  }
+}
