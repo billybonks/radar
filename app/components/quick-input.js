c7049161167153a7188d415e215edbe891bc1fe5
billybonksl <sebastienstettler@gmail.com>
2021-11-28 15:10:52 +0800

    refactor: Abstract quick input
    I plan to use it as part of widget selection on the dashboard, in
    order to do that i need to parameterise the component with the array of
    selections and do something on clic

--- app/components/quick-input.js
+++ app/components/quick-input.js
@@ -1,34 +1,16 @@
 //https://github.com/microsoft/vscode/blob/9e5ee5521a2142d4314660ce1d022082e61f137a/src/vs/base/parts/quickinput/browser/quickInput.ts
 import Component from '@glimmer/component';
-import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
-import { task } from 'ember-concurrency';
 import { tracked } from '@glimmer/tracking';
 
 export default class QuickInputComponent extends Component {
   @tracked filteredCommands;
-  @service commands;
-  @service router;
-  @service store;
 
   constructor() {
     super(...arguments);
-    this.filterCommands.perform();
-  }
-
-  @task *filterCommands() {
-    let charts = yield this.store.findAll('chart');
-    let chartCommands = charts
-      .filter((q) => !q.isNew)
-      .map((query) => {
-        return {
-          title: `Chart: ${query.name}`,
-          callback(router) {
-            router.transitionTo('query.edit', query);
-          },
-        };
-      });
-    this.filteredCommands = [...this.commands.pallete, ...chartCommands];
+    this.args.filterCommands.perform().then((result) => {
+      this.filteredCommands = result;
+    });
   }
 
   valueChanged(event) {
@@ -36,8 +18,7 @@ export default class QuickInputComponent extends Component {
   }
 
   @action
-  callCommand(command) {
-    command.callback(this.router);
-    this.args.onSelected();
+  onSelected(command) {
+    this.args.onSelected(command);
   }
 }
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,11 +1,15 @@
 import Route from '@ember/routing/route';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-
+import { inject as service } from '@ember/service';
 import { bindKeyboardShortcuts } from 'ember-keyboard-shortcuts';
-
+import { task } from 'ember-concurrency';
 export default class ApplicationController extends Route {
   @tracked displayQuickInput = false;
+  @service router;
+  @service store;
+  @service commands;
+
   word = 'qqq';
   keyboardShortcuts = {
     // trigger 'cancel' action when esc is pressed
@@ -27,11 +31,31 @@ export default class ApplicationController extends Route {
     bindKeyboardShortcuts(this);
   }
 
+  @task *filterCommands() {
+    let charts = yield this.store.findAll('chart');
+    let chartCommands = charts
+      .filter((q) => !q.isNew)
+      .map((query) => {
+        return {
+          title: `Chart: ${query.name}`,
+          callback(router) {
+            router.transitionTo('graph.edit', query);
+          },
+        };
+      });
+    return [...this.commands.pallete, ...chartCommands];
+  }
+
   openQuickInput() {
     this.displayQuickInput = true;
   }
 
   @action
+  quickInputSelection(command) {
+    command.callback(this.router);
+    this.closeQuickInput();
+  }
+
   closeQuickInput() {
     this.displayQuickInput = false;
   }
