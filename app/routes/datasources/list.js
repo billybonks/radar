bbdff5ced5707c1690d042c1cfbb5ee850e62bd7
billybonksl <sebastienstettler@gmail.com>
2021-12-17 23:50:29 +0400

    explicit injection of store

--- app/routes/datasources/list.js
+++ app/routes/datasources/list.js
@@ -1,6 +1,8 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class DatasourcesListRoute extends Route {
+  @service store;
   model() {
     return this.store.findAll('datasource');
   }
