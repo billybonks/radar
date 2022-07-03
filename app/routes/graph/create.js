02de170f1195145d147bb5a764ce8ea3e46254ed
billybonksl <sebastienstettler@gmail.com>
2022-07-03 11:22:48 +0800

    add default values when entering create route

--- app/routes/graph/create.js
+++ app/routes/graph/create.js
@@ -4,10 +4,16 @@ import { inject as service } from '@ember/service';
 export default class GraphCreateRoute extends Route {
   @service store;
 
-  model() {
+  async model() {
     let chart = this.store.createRecord('chart');
     let dataset = this.store.createRecord('dataset');
     chart.set('dataset', dataset);
+    chart.set('name', 'Untitled Chart');
+    let datasources = await this.store.findAll('datasource');
+    let visualisations = this.store.peekAll('visualisation');
+    //this needs to be the last obe used in order to do that we need to save the values
+    dataset.set('datasource', datasources.firstObject);
+    chart.changeVis(visualisations.findBy('id', 'table'));
     return chart;
   }
 }
