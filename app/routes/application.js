d49a59b074f6ba900863504b2813d8ff33e0c4db
billybonksl <sebastienstettler@gmail.com>
2022-07-09 14:33:56 +0800

    add home screen with bunch of models for easy viewing

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,24 +1,18 @@
 import Route from '@ember/routing/route';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-import {
-  changeset,
-  parse,
-  View,
-  None,
-  Error as VegaLogError,
-  Warn,
-  Info,
-  Debug,
-} from 'vega';
 
 export default class ApplicationRoute extends Route {
   @service router;
-  beforeModel(transition) {
-    if (transition.to.name === 'index') {
-      this.router.transitionTo('graph.create');
+  @service store;
+
+  async model() {
+    return {
+      datasets: await this.store.findAll('dataset'),
+      charts: await this.store.findAll('chart')
     }
   }
+
   setupController(controller, model) {
     super.setupController(controller, model);
     this.controller = controller;
