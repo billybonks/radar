cca431fa573773b958f4f5385fcd2983fd2d4b72
billybonksl <sebastienstettler@gmail.com>
2022-07-11 13:29:35 +0800

    without this find code it exploded on refresh

--- app/routes/graph/edit.js
+++ app/routes/graph/edit.js
@@ -1,3 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class GraphEditRoute extends Route {}
+export default class GraphEditRoute extends Route {
+  @service store;
+
+  model({ chart_id }) {
+    return this.store.find('chart', chart_id);
+  }
+}
